1a417caf024f6389b62c8ab4df660944b841ef26
Victor Higaki <victorhigaki@outlook.com>
2024-02-04 22:47:23 -0300

    Definindo o conceito de entidade

---
+++ src/DevIO.Business/Models/Fornecedores/Endereco.cs
@@ -0,0 +1,8 @@
+﻿using DevIO.Business.Core.Models;
+
+namespace DevIO.Business.Models.Fornecedores
+{
+    public class Endereco : Entity
+    {
+    }
+}
---
+++ src/DevIO.Business/Models/Produtos/Produto.cs
@@ -0,0 +1,8 @@
+﻿using DevIO.Business.Core.Models;
+
+namespace DevIO.Business.Models.Produtos
+{
+    public class Produto : Entity
+    {
+    }
+}
