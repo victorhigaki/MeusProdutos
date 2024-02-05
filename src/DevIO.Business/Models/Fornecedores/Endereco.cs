73d43ef20a1b83dd092cecc076effdf3d9b7130e
Victor Higaki <victorhigaki@outlook.com>
2024-02-04 23:02:10 -0300

    Escrevendo entidades de negócio

--- src/DevIO.Business/Models/Fornecedores/Endereco.cs
+++ src/DevIO.Business/Models/Fornecedores/Endereco.cs
@@ -4,5 +4,15 @@ namespace DevIO.Business.Models.Fornecedores
 {
     public class Endereco : Entity
     {
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Cep { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+
+        /* EF Relations */
+        public Fornecedor Fornecedor { get; set; }
     }
 }
--- src/DevIO.Business/Models/Produtos/Produto.cs
+++ src/DevIO.Business/Models/Produtos/Produto.cs
@@ -1,8 +1,21 @@
 ﻿using DevIO.Business.Core.Models;
+using DevIO.Business.Models.Fornecedores;
+using System;
+using System.Collections.Generic;
 
 namespace DevIO.Business.Models.Produtos
 {
     public class Produto : Entity
     {
+        public Guid FornecedorId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Imagem { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+
+        /* EF Relations */
+        public Fornecedor Fornecedor { get; set; }
     }
 }
