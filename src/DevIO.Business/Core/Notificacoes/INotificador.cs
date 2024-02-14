d4b9aa5a594d26ee022f2703f05d76e80339a846
Victor Higaki <victorhigaki@outlook.com>
2024-02-14 14:44:39 -0300

    Reapply "Notificação de erros"
    This reverts commit 2907de1eab627ef4ddfb51874b6e79700256a826.

---
+++ src/DevIO.Business/Core/Notificacoes/INotificador.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DevIO.Business.Core.Notificacoes
+{
+    public interface INotificador
+    {
+        bool TemNotificacao();
+        List<Notificacao> ObterNotificacoes();
+        void Handle(Notificacao notificacao);
+    }
+}
