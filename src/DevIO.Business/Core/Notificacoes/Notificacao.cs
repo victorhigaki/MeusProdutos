e22e35dce63cb329deb539507670ed372f0e46cd
Victor Higaki <victorhigaki@outlook.com>
2024-02-14 14:41:32 -0300

    Notificação de erros

---
+++ src/DevIO.Business/Core/Notificacoes/Notificacao.cs
@@ -0,0 +1,12 @@
+﻿namespace DevIO.Business.Core.Notificacoes
+{
+    public class Notificacao
+    {
+        public Notificacao(string mensagem)
+        {
+            Mensagem = mensagem;
+        }
+
+        public string Mensagem { get; }
+    }
+}
