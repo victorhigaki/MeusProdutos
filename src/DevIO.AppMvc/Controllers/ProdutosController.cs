fdee6e13261554a5a423fce1ba7b52176e9fdeb7
Victor Higaki <victorhigaki@outlook.com>
2024-02-17 13:29:09 -0300

    Trabalhando com Scaffolding

---
+++ src/DevIO.AppMvc/Controllers/ProdutosController.cs
@@ -0,0 +1,131 @@
+﻿using DevIO.AppMvc.ViewModels;
+using DevIO.Business.Core.Notificacoes;
+using DevIO.Business.Models.Produtos;
+using DevIO.Business.Models.Produtos.Services;
+using DevIO.Infra.Data.Repository;
+using System;
+using System.Data.Entity;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace DevIO.AppMvc.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private readonly IProdutoRepository _produtoRepository;
+        private readonly IProdutoService _produtoService;
+
+        public ProdutosController()
+        {
+            _produtoRepository = new ProdutoRepository();
+            _produtoService = new ProdutoService(_produtoRepository, new Notificador());
+        }
+
+        // GET: Produtos
+        public async Task<ActionResult> Index()
+        {
+            return View(await _produtoRepository.ObterTodos());
+        }
+
+        // GET: Produtos/Details/5
+        public async Task<ActionResult> Details(Guid id)
+        {
+            var produto = await _produtoRepository.ObterPorId(id);
+
+            if (produtoViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produtoViewModel);
+        }
+
+        // GET: Produtos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Produtos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(ProdutoViewModel produtoViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _produtoService.Adicionar(produtoViewModel);
+                return RedirectToAction("Index");
+            }
+
+            return View(produtoViewModel);
+        }
+
+        // GET: Produtos/Edit/5
+        public async Task<ActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProdutoViewModel produtoViewModel = await db.ProdutoViewModels.FindAsync(id);
+            if (produtoViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produtoViewModel);
+        }
+
+        // POST: Produtos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,FornecedorId,Nome,Descricao,Imagem,Valor,DataCadastro,Ativo")] ProdutoViewModel produtoViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(produtoViewModel).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(produtoViewModel);
+        }
+
+        // GET: Produtos/Delete/5
+        public async Task<ActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProdutoViewModel produtoViewModel = await db.ProdutoViewModels.FindAsync(id);
+            if (produtoViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produtoViewModel);
+        }
+
+        // POST: Produtos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(Guid id)
+        {
+            ProdutoViewModel produtoViewModel = await db.ProdutoViewModels.FindAsync(id);
+            db.ProdutoViewModels.Remove(produtoViewModel);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- src/DevIO.AppMvc/ViewModels/ProdutoViewModel.cs
+++ src/DevIO.AppMvc/ViewModels/ProdutoViewModel.cs
@@ -30,8 +30,8 @@ namespace DevIO.AppMvc.ViewModels
         [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Descricao { get; set; }
 
-        [DisplayName("Imagem do Produto")]
-        public HttpPostedFileBase ImagemUpload { get; set; }
+        //[DisplayName("Imagem do Produto")]
+        //public HttpPostedFileBase ImagemUpload { get; set; }
 
         public string Imagem { get; set; }
 
@@ -43,7 +43,7 @@ namespace DevIO.AppMvc.ViewModels
 
         [DisplayName("Ativo?")]
         public bool Ativo { get; set; }
-        public FornecedorViewModel Fornecedor { get; set; }
-        public IEnumerable<FornecedorViewModel> Fornecedores { get; set; }
+        //public FornecedorViewModel Fornecedor { get; set; }
+        //public IEnumerable<FornecedorViewModel> Fornecedores { get; set; }
     }
 }
