7e34585e7349699e3d7a3eee9ed7eaa6cb3d50de
Victor Higaki <victorhigaki@outlook.com>
2024-02-17 18:10:57 -0300

    Utilizando o Startup.cs da forma certa

--- src/DevIO.AppMvc/Global.asax.cs
+++ src/DevIO.AppMvc/Global.asax.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -8,14 +5,12 @@ using System.Web.Routing;
 
 namespace DevIO.AppMvc
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
         }
     }
 }
--- src/DevIO.AppMvc/Startup.cs
+++ src/DevIO.AppMvc/Startup.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Owin;
+﻿using DevIO.AppMvc.App_Start;
+using Microsoft.Owin;
 using Owin;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
 
 [assembly: OwinStartupAttribute(typeof(DevIO.AppMvc.Startup))]
 namespace DevIO.AppMvc
@@ -9,6 +13,13 @@ namespace DevIO.AppMvc
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+
+            DependencyInjectionConfig.RegisterDIContainer();
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
         }
     }
 }
