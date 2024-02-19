5dce5f92a7f8a8fa12d4d5e2d34e2c692f5057f8
Victor Higaki <victorhigaki@outlook.com>
2024-02-19 06:52:07 -0300

    Implementando a Controller Fornecedores

--- src/DevIO.AppMvc/Controllers/FornecedoresController.cs
+++ src/DevIO.AppMvc/Controllers/FornecedoresController.cs
@@ -1,44 +1,118 @@
-﻿using DevIO.Business.Models.Fornecedores;
-using DevIO.Business.Models.Fornecedores.Services;
-using DevIO.Infra.Data.Repository;
+﻿using AutoMapper;
+using DevIO.AppMvc.ViewModels;
+using DevIO.Business.Models.Fornecedores;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace DevIO.AppMvc.Controllers
 {
-    public class FornecedoresController : Controller
+    public class FornecedoresController : BaseController
     {
-        private readonly IFornecedorService _fornecedorService;
+        private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IFornecedorRepository _fornecedorService;
+        private readonly IMapper _mapper;
 
-        public FornecedoresController()
+        public FornecedoresController(IFornecedorRepository fornecedorRepository,
+                                      IFornecedorRepository fornecedorService,
+                                      IMapper mapper)
         {
-            //_fornecedorService = new FornecedorService(new FornecedorRepository(), new EnderecoRepository());
+            _fornecedorRepository = fornecedorRepository;
+            _fornecedorService = fornecedorService;
+            _mapper = mapper;
         }
 
-        // GET: Fornecedores
+        [Route("lista-fornecedores")]
         public async Task<ActionResult> Index()
         {
-            //var fornecedor = new Fornecedor()
-            //{
-            //    Nome = "Eduardo",
-            //    Documento = "30390600822",
-            //    Endereco = new Endereco
-            //    {
-            //        Logradouro = "Rua Teste",
-            //        Numero = "123",
-            //        Complemento = "Teste",
-            //        Bairro = "Teste",
-            //        Cep = "12345678",
-            //        Cidade = "Teste",
-            //        Estado = "Teste",
-            //    },
-            //    TipoFornecedor = TipoFornecedor.PessoaFisica,
-            //    Ativo = true
-            //};
+            return View(_mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos()));
+        }
 
-            //await _fornecedorService.Adicionar(fornecedor);
+        [Route("novo-fornecedor")]
+        public ActionResult Create()
+        {
+            return View();
+        }
 
-            return new EmptyResult();
+        [Route("novo-fornecedor")]
+        [HttpPost]
+        public async Task<ActionResult> Create(FornecedorViewModel fornecedorViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(fornecedorViewModel);
+            }
+
+            var fornecedor = _mapper.Map<Fornecedor>(fornecedorViewModel);
+
+            await _fornecedorService.Adicionar(fornecedor);
+
+            return RedirectToAction("Index");
+        }
+
+        [Route("editar-fornecedor/{id:guid}")]
+        public async Task<ActionResult> Editar(Guid id)
+        {
+            var fornecedorViewModel = await ObterFornecedorEndereco(id);
+
+            if (fornecedorViewModel == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(fornecedorViewModel);
+        }
+
+        [Route("editar-fornecedor/{id:guid}")]
+        [HttpPost]
+        public async Task<ActionResult> Editar(Guid id, FornecedorViewModel fornecedorViewModel)
+        {
+            if (id != fornecedorViewModel.Id) return HttpNotFound();
+
+            if (!ModelState.IsValid) return View(fornecedorViewModel);
+
+            var fornecedor = _mapper.Map<Fornecedor>(fornecedorViewModel);
+            await _fornecedorService.Atualizar(fornecedor);
+
+            return RedirectToAction("Index");
+        }
+
+        [Route("excluir-fornecedor/{id:guid}")]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            var fornecedorViewModel = await ObterFornecedorEndereco(id);
+
+            if (fornecedorViewModel == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(fornecedorViewModel);
+        }
+
+        [Route("excluir-fornecedor/{id:guid}")]
+        [HttpPost, ActionName("Delete")]
+        public async Task<ActionResult> DeleteConfirmed(Guid id)
+        {
+            var fornecedor = await ObterFornecedorEndereco(id);
+
+            if (fornecedor == null) return HttpNotFound();
+
+            await _fornecedorService.Remover(id);
+
+            return RedirectToAction("Index");
+        }
+
+
+        private async Task<FornecedorViewModel> ObterFornecedorEndereco(Guid id)
+        {
+            return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorEndereco(id));
+        }
+
+        private async Task<FornecedorViewModel> ObterFornecedorProdutosEndereco(Guid id)
+        {
+            return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorProdutosEndereco(id));
         }
     }
 }
