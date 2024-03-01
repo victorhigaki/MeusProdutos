21e9781a3b9ce845ca0e890a7ec7c03473fb91d3
Victor Higaki <victorhigaki@outlook.com>
2024-02-29 22:47:38 -0300

    Trabalhando com JavaScript e JQuery

--- src/DevIO.AppMvc/App_Start/BundleConfig.cs
+++ src/DevIO.AppMvc/App_Start/BundleConfig.cs
@@ -22,6 +22,9 @@ namespace DevIO.AppMvc
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/site").Include(
+                        "~/Scripts/site.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
