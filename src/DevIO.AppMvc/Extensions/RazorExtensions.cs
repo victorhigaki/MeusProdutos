9901adb15d85ab6b435a195dfc3cc6486099d630
Victor Higaki <victorhigaki@outlook.com>
2024-03-13 23:08:38 -0300

    Implementando extensões do Razor

---
+++ src/DevIO.AppMvc/Extensions/RazorExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevIO.AppMvc.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static string FormatarDocumento(this WebViewPage page, int tipoPessoa, string documento)
+        {
+            return tipoPessoa == 1
+                ? Convert.ToUInt64(documento).ToString(@"000\.000\.000\-00")
+                : Convert.ToUInt64(documento).ToString(@"00\.000\.000\/0000\-00");
+        }
+
+        public static bool ExibirNaURL(this WebViewPage value, Guid id)
+        {
+            var urlHelper = new UrlHelper(HttpContext.Current.Request.RequestContext);
+            var urlTarget = urlHelper.Action("Edit", "Fornecedores", new { id = id });
+            var urlTarget2 = urlHelper.Action("ObterEndereco", "Fornecedores", new { id = id });
+
+            var urlEmUso = HttpContext.Current.Request.Path;
+
+            return urlTarget == urlEmUso || urlTarget2 == urlEmUso;
+        }
+    }
+}
