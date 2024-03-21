ab281a57e5df486b3f164f52c860d872ef03a822
Victor Higaki <victorhigaki@outlook.com>
2024-03-21 00:00:33 -0300

    Definindo data automática no EF

--- src/DevIO.Infra/Data/Context/MeuDbContext.cs
+++ src/DevIO.Infra/Data/Context/MeuDbContext.cs
@@ -1,15 +1,19 @@
 ﻿using DevIO.Business.Models.Fornecedores;
 using DevIO.Business.Models.Produtos;
 using DevIO.Infra.Data.Mappings;
+using System;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace DevIO.Infra.Data.Context
 {
     public class MeuDbContext : DbContext
     {
         public MeuDbContext() : base("DefaultConnection")
-        { 
+        {
             Configuration.ProxyCreationEnabled = false;
             Configuration.LazyLoadingEnabled = false;
         }
@@ -34,6 +38,24 @@ namespace DevIO.Infra.Data.Context
             modelBuilder.Configurations.Add(new ProdutoConfig());
 
             base.OnModelCreating(modelBuilder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
         }
     }
 }
