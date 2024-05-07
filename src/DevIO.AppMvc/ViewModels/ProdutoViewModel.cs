bd66b98512403c14be4c0efb759efdf472e9d007
Victor Higaki <victorhigaki@outlook.com>
2024-05-06 23:06:27 -0300

    Upload de imagens - front-end

--- src/DevIO.AppMvc/ViewModels/ProdutoViewModel.cs
+++ src/DevIO.AppMvc/ViewModels/ProdutoViewModel.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Web;
 
 namespace DevIO.AppMvc.ViewModels
 {
@@ -29,8 +30,8 @@ namespace DevIO.AppMvc.ViewModels
         [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Descricao { get; set; }
 
-        //[DisplayName("Imagem do Produto")]
-        //public HttpPostedFileBase ImagemUpload { get; set; }
+        [DisplayName("Imagem do Produto")]
+        public HttpPostedFileBase ImagemUpload { get; set; }
 
         public string Imagem { get; set; }
 
